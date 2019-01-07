661fab38a083420449a542773d08f794a0e27eba
Veit Karpf <veit.karpf@gmx.net>
2019-01-07 13:36:45 +0100

    display setup on desktop again horizontal

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -16,27 +16,21 @@ if [ ${name} = "thinkpadx260" ]; then
   exit 0
 fi
 
-if [ ${name} = "desktop" ]; then
-  polybar --reload desktop_wide &
-  polybar --reload desktop_high &
-  exit 0
-fi
-
 if [ ${name} = "thinkpadx230" ]; then
   polybar --reload thinkpadx230 &
   exit 0
 fi
 
-sleep 1
+sleep 0.1
 
 # Launch on multi monitor
-#if type "xrandr"; then
-#  for monitor in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-#    MONITOR=${monitor} polybar --reload ${name}& # -c $HOME/.config/polybar/config &
-#  done
-#else
-#  polybar --reload ${name}& # -c $HOME/.config/polybar/config &
-#fi
+if type "xrandr"; then
+  for monitor in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=${monitor} polybar --reload ${name}& # -c $HOME/.config/polybar/config &
+  done
+else
+  polybar --reload ${name}& # -c $HOME/.config/polybar/config &
+fi
 
 # Launch bar1 and bar2
 #polybar -r bar1
